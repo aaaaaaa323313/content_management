3e7ac8e2a3db04fce56700076373e09d2541e5c0
gaoguanyu <springaction@foxmail.com>
2014-08-19 19:16:09 +0800

    d1

--- client.py
+++ client.py
@@ -2,10 +2,13 @@ import datetime
 import xmlrpclib
 
 proxy = xmlrpclib.ServerProxy("http://localhost:10002/")
-print "okey -1"
+
 with open("dream_000.ts", "rb") as handle:
     ts = xmlrpclib.Binary(handle.read())
-    print "okey -2"
+
+width   = '400'
+height  = '400'
+br      = '1k'
 
 with open("dream_000_test.ts", "wb") as handle:
-    handle.write(proxy.python_logo(ts).data)
+    handle.write(proxy.python_logo(ts, width, height, br).data)
--- server.py
+++ server.py
@@ -1,10 +1,32 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+import os
+import random
+import string
 import xmlrpclib
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-def python_logo(ts):
-    #with open("dream_000.ts", "rb") as handle:
-    #    return xmlrpclib.Binary(handle.read())
-    return ts
+def random_string(length):
+    return ''.join(random.choice(string.lowercase) \
+            for i in range(length))
+
+def python_logo(segment, width, height, br):
+    file_in     = random_string(10) + "_in_" + ".ts"
+    file_out    = random_string(10) + "_out_" + ".ts"
+
+    with open(file_in, "wb") as handle:
+        handle.write(segment.data)
+    print 'width:',     width
+    print 'height:',    height
+    print 'bitrate:',   br
+
+    cmd = "ffmpeg -y -i " + file_in + " -s " \
+            + width + "x" + height + " " + file_out
+    print cmd
+    os.system(cmd)
+
+    with open(file_out, "rb") as handle:
+        trans_segment = xmlrpclib.Binary(handle.read())
+
+    return trans_segment
 
 server = SimpleXMLRPCServer(("localhost", 10002))
 print "Listening on port 8000..."
